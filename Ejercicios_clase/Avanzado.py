fcfd309fca17a2b0b584dfa4455fed29c830fb2e
RocioRRdz <114269139+RocioRRdz@users.noreply.github.com>
2023-04-16 13:48:45 -0600

    Add files via upload

---
+++ Ejercicios_clase/Avanzado.py
@@ -0,0 +1,68 @@
+#EJEMPLO: Avanzado
+
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
+
+datos1=[108, 31, 75, 87, 79, 88, 89, 118, 51, 89, 174, 95, 51, 70, 73]
+datos2=[180, 31, 75, 190, 79, 88, 89, 118, 51, 89, 174, 95, 51, 70, 73]
+
+datos =[datos1, datos2]
+
+# Create 2x2 sub plots
+gs = gridspec.GridSpec(2, 2)
+figure = plt.figure(figsize=(12, 7))
+
+ax = figure.add_subplot(gs[0, :])
+bp = ax.boxplot(datos,
+                patch_artist = True, #fill with color
+                vert = False, #vertical box alignment
+                labels = ["Grafica 1", "Grafica 2"]) #will be used to label ticks
+ax.set_title("Boxplot DEFAULT")
+
+colors = ["Lime", "blue"]
+for patch, color in zip(bp["boxes"], colors):
+    patch.set_facecolor(color)
+
+ax = figure.add_subplot(gs[1, 0])
+bp = ax.boxplot(datos,
+                patch_artist = True, #Fill with color
+                vert = False, #vertical box alignment
+                labels=["Grafica 1", "Grafica 2"]) #will be used to label ticks
+ax.set_title("soxplot Mhis de 1.5")
+
+#changing color and linewidth of
+#whiskers
+for whisker in bp["whiskers"]:
+    whisker.set(color ="#8B008B",
+                linewidth = 1.5,
+                linestyle =":")
+
+#changing color and Linewidth of
+#caps
+for cap in bp["caps"]:
+    cap.set(color ="red",
+            linewidth = 2)
+
+ax = figure.add_subplot(gs[1, 1])
+bp = ax.boxplot(datos, whis= 3.0,
+                patch_artist = True, #fill with color
+                vert = False, #vertical box aligment
+                labels=["Gráfica 1", "Gráfica 2"]) #will be used to label ticks
+ax.set_title("Boxplot whis de 3.0")
+
+#changing color and linewidth of
+#medians
+for median in bp["medians"]:
+    median.set(color ="aqua",
+                linewidth = 3)
+
+#changing style of fliers
+for flier in bp["fliers"]:
+    flier.set(marker = "D",
+                color ="#e7298a",
+                alpha = 0.5)
+
+# x-axis labels
+ax.set_yticklabels(['datos 1', "datos 2"])
+
+plt.show()
---
+++ Ejercicios_clase/Basico.py
@@ -0,0 +1,8 @@
+#EJEMPLO: Básico.
+
+import matplotlib.pyplot as plt
+datos=[108, 31, 75, 87, 79, 88, 89, 118, 51, 89, 174, 95, 51, 70, 73]
+plt.boxplot(datos)
+
+plt.title("Boxplot Básico")
+plt.show()

fc5ec9a7217687c8b88b0243bf61f99d32583117
RocioRRdz <114269139+RocioRRdz@users.noreply.github.com>
2023-05-20 04:18:56 -0600

    Delete Puntuacion_z.py

--- Puntuacion_z.py
+++
@@ -1,9 +0,0 @@
-import statistics
-
-def calcular_puntuaciones_z(datos):
-    media = statistics.mean(datos)
-    desviacion_estandar = statistics.stdev(datos)
-
-    puntuaciones_z = [(x - media) / desviacion_estandar for x in datos]
-    return puntuaciones_z
-
